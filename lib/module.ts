6bba6e1f5e6592014dced985ae1050c697e80d42
Will Soto <willsoto@users.noreply.github.com>
2020-07-10 08:44:22 -0400

    chore(module): remove deprecated forRoot and forRootAsync methods
    BREAKING CHANGE: forRoot and forRootAsync methods no longer exist.
    Please start using register and registerAsync instead.

--- lib/module.ts
+++ lib/module.ts
@@ -1,5 +1,5 @@
 /* eslint-disable new-cap */
-import { DynamicModule, Logger, Module, Provider } from "@nestjs/common";
+import { DynamicModule, Module, Provider } from "@nestjs/common";
 import { Model } from "objection";
 import { ObjectionCoreModule } from "./core";
 import {
@@ -9,29 +9,12 @@ import {
 
 @Module({})
 export class ObjectionModule {
-  public static forRoot(options: ObjectionModuleOptions): DynamicModule {
-    Logger.warn(
-      "ObjectionModule#forRoot has been deprecated and will be removed in the next major version. Please use ObjectionModule#register instead.",
-    );
-    return ObjectionModule.register(options);
-  }
-
   public static register(options: ObjectionModuleOptions): DynamicModule {
     return {
       module: ObjectionModule,
       imports: [ObjectionCoreModule.register(options)],
       exports: [ObjectionCoreModule],
     };
-  }
-
-  public static forRootAsync(
-    options: ObjectionModuleAsyncOptions,
-  ): DynamicModule {
-    Logger.warn(
-      "ObjectionModule#forRootAsync has been deprecated and will be removed in the next major version. Please use ObjectionModule#registerAsync instead.",
-    );
-
-    return ObjectionModule.registerAsync(options);
   }
 
   public static registerAsync(
