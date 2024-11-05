a6d6c22ceb6547e4bf6d4d8ef9fb7ad657320ca5
Will Soto <willsoto@users.noreply.github.com>
2024-11-04 20:11:59 -0500

    chore: migrate to eslint v8
    Signed-off-by: Will Soto <willsoto@users.noreply.github.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,69 @@
+/* eslint-disable */
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ["**/node_modules", "**/dist", "**/coverage", "**/typings"],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ),
+  {
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: "commonjs",
+
+      parserOptions: {
+        project: "./tsconfig.lint.json",
+      },
+    },
+  },
+  ...compat.extends("plugin:mocha/recommended").map((config) => ({
+    ...config,
+    files: ["test/**/*.ts"],
+  })),
+  {
+    files: ["test/**/*.ts"],
+
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+
+    rules: {
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "no-unused-expressions": "off",
+      "no-magic-numbers": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+    },
+  },
+];
